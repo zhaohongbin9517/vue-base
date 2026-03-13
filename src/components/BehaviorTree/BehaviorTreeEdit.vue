5d9adbf7145075ab471c2d3c1ed7a8c35578bd64
zhaohongbin <1458286899@qq.com>
2026-03-13 11:14:25 +0800

    更新

--- src/components/BehaviorTree/BehaviorTreeEdit.vue
+++ src/components/BehaviorTree/BehaviorTreeEdit.vue
@@ -1,89 +1,109 @@
 <template>
-  <el-card class="config-card" shadow="hover">
-    <template #header>
-      <div class="card-header">
-        <el-icon class="header-icon"><tools /></el-icon>
-        <span>行为树编辑</span>
-        <div class="param-btn">
-          <el-button type="primary" size="large">
-            <el-icon><plus /></el-icon>
-            保存行为树
-          </el-button>
-        </div>
-      </div>
+    <VueFlow :nodes="nodes" :edges="edges" fit-view-on-init class="behavior-tree-edit"> 
+    <template #edge-button="buttonEdgeProps">
+      <EdgeWithButton
+        :id="buttonEdgeProps.id"
+        :source-x="buttonEdgeProps.sourceX"
+        :source-y="buttonEdgeProps.sourceY"
+        :target-x="buttonEdgeProps.targetX"
+        :target-y="buttonEdgeProps.targetY"
+        :source-position="buttonEdgeProps.sourcePosition"
+        :target-position="buttonEdgeProps.targetPosition"
+        :marker-end="buttonEdgeProps.markerEnd"
+        :style="buttonEdgeProps.style"
+      />
     </template>
-    <!--
-      行为树编辑容器
-    -->
-  </el-card>
+
+    <template #edge-custom="customEdgeProps">
+      <CustomEdge
+        :id="customEdgeProps.id"
+        :source-x="customEdgeProps.sourceX"
+        :source-y="customEdgeProps.sourceY"
+        :target-x="customEdgeProps.targetX"
+        :target-y="customEdgeProps.targetY"
+        :source-position="customEdgeProps.sourcePosition"
+        :target-position="customEdgeProps.targetPosition"
+        :data="customEdgeProps.data"
+        :marker-end="customEdgeProps.markerEnd"
+        :style="customEdgeProps.style"
+      />
+    </template>
+
+    <Background />
+  </VueFlow>
 </template>
 
-<script>
-import { Tools, Plus } from '@element-plus/icons-vue'
-export default {
-  name: 'MeterConfigResultSetting',
-  components: {
-    Tools,
-    Plus
+<script setup>
+import { h, ref } from 'vue'
+import { Background } from '@vue-flow/background'
+import { MarkerType, VueFlow } from '@vue-flow/core'
+import EdgeWithButton from './BehaviorTreeEditChile/EdgeWithButton.vue'
+import CustomEdge from './BehaviorTreeEditChile/CustomEdge.vue'
+import CustomEdgeLabel from './BehaviorTreeEditChile/CustomEdgeLabel.vue'
+
+const nodes = ref([
+  { id: '1', type: 'input', label: 'Start', position: { x: 50, y: 0 }, style: { borderColor: '#10b981' } },
+  { id: '2', label: 'Node 2', position: { x: 150, y: 100 } },
+  { id: '2a', label: 'Node 2a', position: { x: 0, y: 180 } },
+  { id: '3', label: 'Node 3', position: { x: 250, y: 200 } },
+  { id: '4', label: 'Node 4', position: { x: 400, y: 300 } },
+  { id: '3a', label: 'Node 3a', position: { x: 175, y: 300 } },
+  { id: '5', label: 'Node 5', position: { x: 200, y: 400 } },
+  { id: '6', type: 'output', label: 'Output 6', position: { x: 0, y: 350 } },
+  { id: '7', type: 'output', label: 'Output 7', position: { x: 50, y: 600 } },
+  { id: '8', type: 'output', label: 'Output 8', position: { x: 350, y: 600 } },
+  { id: '9', type: 'output', label: 'Output 9', position: { x: 550, y: 400 } },
+])
+
+const edges = ref([
+  { id: 'e1-2', source: '1', target: '2', label: 'bezier edge (default)', class: 'normal-edge' },
+  { id: 'e2-2a', source: '2', target: '2a', type: 'smoothstep', label: 'smoothstep edge' },
+  { id: 'e2-3', source: '2', target: '3', type: 'step', label: 'step edge' },
+  { id: 'e3-4', source: '3', target: '4', type: 'straight', label: 'straight edge' },
+  { id: 'e3-3a', source: '3', target: '3a', type: 'straight', label: 'label only edge', style: { stroke: 'none' } },
+  { id: 'e3-5', source: '4', target: '5', animated: true, label: 'animated styled edge', style: { stroke: '#10b981' } },
+  {
+    id: 'e2a-6',
+    source: '2a',
+    target: '6',
+    label: () => h(CustomEdgeLabel, { label: 'custom label text' }),
+    labelStyle: { fill: '#10b981', fontWeight: 700 },
+    markerEnd: MarkerType.Arrow,
   },
-  data() {
-    return {
-    }
+  {
+    id: 'e5-7',
+    source: '5',
+    target: '7',
+    label: 'label with bg',
+    labelBgPadding: [8, 4],
+    labelBgBorderRadius: 4,
+    labelBgStyle: { fill: '#FFCC00', color: '#fff', fillOpacity: 0.7 },
+    markerEnd: MarkerType.ArrowClosed,
   },
-  mounted() {
+  {
+    id: 'e5-8',
+    source: '5',
+    target: '8',
+    type: 'button',
+    data: { text: 'custom edge' },
+    markerEnd: MarkerType.ArrowClosed,
   },
-  
-  methods: {
-  }
-}
+  {
+    id: 'e4-9',
+    source: '4',
+    target: '9',
+    type: 'custom',
+    data: { text: 'styled custom edge label' },
+  },
+])
 </script>
 
 <style scoped>
-.config-card {
-  min-height: 83vh;
-  max-height: 83vh;
+.behavior-tree-edit {
+  min-height: 81vh;
+  max-height: 81vh;
   overflow-y: auto;
-  margin-bottom: px;
+  margin-bottom: 20px;
   border-radius: 8px;
 }
-:deep(.el-card__header) {
-  padding: 12px 20px;
-}
-:deep(.el-card__body) {
-  /* 隐藏滚动条 - Chrome, Safari, Edge */
-  scrollbar-width: none;
-  -ms-overflow-style: none;
-}
-.group-container:-webkit-scrollbar {
-  display: none;
-}
-
-.card-header {
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  font-size: 16px;
-  font-weight: 600;
-  color: #303133;
-}
-
-
-
-.header-icon {
-  font-size: 20px;
-  color: #409eff;
-}
-
-.tips-text {
-  font-size: 12px;
-  color: #409eff;
-  margin-left: 8px;
-}
-
-.param-btn {
-  margin-left: auto;
-  display: flex;
-  gap: 10px;
-}
-
 </style>
---
+++ src/components/BehaviorTree/BehaviorTreeEditChile/CustomEdgeLabel.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <tspan dy="10" x="0">{{ props.label }}</tspan>
+</template>
+
